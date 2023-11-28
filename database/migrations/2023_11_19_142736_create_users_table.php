5b3bb1a7a4ce821b42fccd6fe729e5aee75fcd83
oginoshikibu <oginoshikibu@gmail.com>
2023-11-28 21:54:38 +0900

    crate migration file

--- database/migrations/2023_11_19_142736_create_users_table.php
+++ database/migrations/2023_11_19_142736_create_users_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->id('user_id');
+            $table->string('user_name');
+            $table->string('user_email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ database/migrations/2023_11_19_143007_create_answers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id('answer_id');
+            $table->id('user_id');
+            $table->id('question_id');
+            $table->integer('strokes');
+            $table->json('play_history');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('answers');
+    }
+};
