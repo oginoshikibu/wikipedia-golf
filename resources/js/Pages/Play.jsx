2068b8dead1a10909fe1098fbf972b15f8d8335f
oginoshikibu <oginoshikibu@gmail.com>
2024-01-02 22:50:45 +0900

    inputに入力した文字列ページに飛ぶようにした

--- resources/js/Pages/Play.jsx
+++ resources/js/Pages/Play.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Head } from '@inertiajs/react';
 import PrimaryButton from '@/Components/PrimaryButton';
 import Iframe from '@/Components/Iframe';
+import TextInput from '@/Components/TextInput';
 
 // todo
 // iframe をコンポーネントで作成する
@@ -11,10 +12,29 @@ export default function Play() {
 
     const [src, setSrc] = useState("https://ja.wikipedia.org/wiki/%E3%83%9C%E3%83%BC%E3%83%89%E3%82%B2%E3%83%BC%E3%83%A0");
 
+    const shot = (e) => {
+        e.preventDefault();
+        setSrc("https://ja.wikipedia.org/wiki/" + e.target.shot.value);
+
+    };
+
     return (
         <>
+
             <Head title="Play" />
-            <PrimaryButton onClick={() => setSrc("https://ja.wikipedia.org/wiki/%E3%82%B2%E3%83%BC%E3%83%A0")}>ゲーム</PrimaryButton>
+
+            <form onSubmit={shot}>
+                <TextInput
+                    id="shot"
+                    type="text"
+                    name="shot"
+                    className="mt-1 block w-full"
+                    autoComplete="shot"
+                    onChange={(e) => shot(e.target.value)}
+                />
+                <PrimaryButton>shot!!!</PrimaryButton>
+            </form>
+            
             <Iframe src={src} className="mt-4" width="100%" height="1000px" />
         </>
     );
