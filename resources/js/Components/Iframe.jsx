f78126209d83c0ebefbdf74d3d9bee3b378c2179
oginoshikibu <oginoshikibu@gmail.com>
2024-01-07 23:20:28 +0900

    iframeをcomponentでなく平で実装

--- resources/js/Components/Iframe.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export default function Iframe({ src, className = '', ...props }) {
-
-
-    return (
-        <iframe {...props} src={src} className={className} />
-    );
-}
