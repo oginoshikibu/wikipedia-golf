d7ac56ab2b92eb5b71e6ef7040c6e0f7aea52b5b
oginoshikibu <oginoshikibu@gmail.com>
2024-01-08 14:03:23 +0900

    ざっくりとひな型作成

--- resources/js/Pages/Play.jsx
+++ resources/js/Pages/Play.jsx
@@ -5,10 +5,6 @@ import TextInput from '@/Components/TextInput';
 
 export default function Play({ const: startPageTitle = "ゲーム", const: goalPageTitle = "ゴルフ" }) {
 
-    // mock default value
-    const startUrl = "https://ja.wikipedia.org/wiki/" + startPageTitle;
-    const goalUrl = "https://ja.wikipedia.org/wiki/" + goalPageTitle;
-
     // https://www.mediawiki.org/wiki/API:REST_API/Reference/ja#HTMLの取得 よりコピペ
     async function wikiFetch(title) {
         let url = `https://en.wikipedia.org/w/rest.php/v1/page/${title}/html`;
@@ -31,7 +27,6 @@ export default function Play({ const: startPageTitle = "ゲーム", const: goalP
       }
       
       fetchAsync();
-
     
     const modifyHtmlLinks = (html) => {
         // htmlの中のリンクを変更する
@@ -50,50 +45,21 @@ export default function Play({ const: startPageTitle = "ゲーム", const: goalP
         return page;
     }
 
-
-    const getAllLinks = (wikiUrl) => {
-        // media wiki apiを叩いて、wikiUrlのページのリンクを取得する
-        // https://www.mediawiki.org/wiki/API:Main_page
-        const links = new Set();
-        // debag mock用
-        links.add(goalUrl);
-        links.add("https://ja.wikipedia.org/wiki/テニス");
-        links.add("https://ja.wikipedia.org/wiki/サッカー");
-        links.add("https://ja.wikipedia.org/wiki/野球");
-        return links;
-    }
-
-    const canShot = (currentWikiUrl, nextWikiUrl) => {
-        // currentWikiUrlからnextWikiUrlに移動できるかどうかを判定する
-        // 1. currentWikiUrlのページのリンクを取得する
-        // 2. nextWikiUrlがそのリンクの中にあるかどうかを判定する
-        // 3. あればtrue,なければfalseを返す
-        const links = getAllLinks(currentWikiUrl);
-        return links.has(nextWikiUrl);
-    }
-
-
-    const [src, setSrc] = useState(startUrl);
+    const [currentPage, setCurrentPage] = useState(getWikiPage(startPageTitle));
     const [currentStrokes, setCurrentStrokes] = useState(0);
 
+    
+    const onLinkClickToHit = (e) => {
+        setCurrentStrokes(currentStrokes + 1);
+        setCurrentPage(getWikiPage(e.target.title));
+    }
+
     useEffect(() => {
-        if (src === goalUrl) {
+        if (currentPage.title === goalPageTitle) {
             alert(currentStrokes + "打でゴールしました");
         }
-    }, [src]);
+    }, [currentPage]);
 
-    const shot = (e) => {
-        e.preventDefault();
-        const nextWikiUrl = ("https://ja.wikipedia.org/wiki/" + e.target.shot.value);
-        if (nextWikiUrl === src) {
-            alert("同じページです");
-        } else if (canShot(src, nextWikiUrl)) {
-            setSrc(nextWikiUrl);
-            setCurrentStrokes(currentStrokes + 1);
-        } else {
-            alert("そのページには移動できません");
-        }
-    }
 
     return (
         <>
@@ -103,20 +69,7 @@ export default function Play({ const: startPageTitle = "ゲーム", const: goalP
                 <p>start page: {startPageTitle} {"→"} goal page: {goalPageTitle}</p>
                 <p>現在の打数: {currentStrokes} 打</p>
             </div>
-            <form onSubmit={shot} className='flex items-center m-3'>
-                <TextInput
-                    id="shot"
-                    type="text"
-                    name="shot"
-                    className="block w-[100%] mr-3"
-                    autoComplete="shot"
-                />
-                <PrimaryButton>shot!!!</PrimaryButton>
-            </form>
-
-            <div className='overflow-auto h-[2000px] border-4 m-3'>
-                <iframe src={src} className="m-4 pointer-events-none" width="100%" height="300000px" tabIndex={-1} />
-            </div>
+            <iframe srcDoc={currentPage.html} className="m-4" width="100%" height="300000px" tabIndex={-1} />
         </>
     );
 }
