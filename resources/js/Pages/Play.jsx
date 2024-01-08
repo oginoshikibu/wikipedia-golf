d2b76da821f7329b3cf08c36842c180ed8699264
oginoshikibu <oginoshikibu@gmail.com>
2024-01-08 14:07:12 +0900

    mediawikiコピペ部分をarrow関数に書き換え

--- resources/js/Pages/Play.jsx
+++ resources/js/Pages/Play.jsx
@@ -3,28 +3,23 @@ import { Head } from '@inertiajs/react';
 
 export default function Play({ const: startPageTitle = "ゲーム", const: goalPageTitle = "ゴルフ" }) {
 
-    // https://www.mediawiki.org/wiki/API:REST_API/Reference/ja#HTMLの取得 よりコピペ
-    async function wikiFetch(title) {
+    // https://www.mediawiki.org/wiki/API:REST_API/Reference/ja#HTMLの取得 よりコピペしたものを改変
+    const wikiFetch = async (title) => {
         let url = `https://en.wikipedia.org/w/rest.php/v1/page/${title}/html`;
         let headers = {'Api-User-Agent': 'MediaWiki REST API docs examples/0.1 (https://www.mediawiki.org/wiki/API_talk:REST_API)'}
-        
-      
         const rsp = await fetch(url, headers);
         const data = await rsp.json();
         return data;
       }
-      
-      async function fetchAsync()
-      {
+    
+    const wikiFetchAsync = async (title) => {
         try {
-          let result = await doFetch();
-          console.log(result);
+            let result = await wikiFetch(title);
+            console.log(result);
         } catch( err ) {
-          console.error( err.message );
+            console.error( err.message );
         }
-      }
-      
-      fetchAsync();
+    }
     
     const modifyHtmlLinks = (html) => {
         // htmlの中のリンクを変更する
@@ -39,7 +34,7 @@ export default function Play({ const: startPageTitle = "ゲーム", const: goalP
         // https://www.mediawiki.org/wiki/API:Main_page
         const page = new Object();
         page.title = title;
-        page.html = modifyHtmlLinks(await wikiFetch(title));
+        page.html = modifyHtmlLinks(wikiFetchAsync(title));
         return page;
     }
 
