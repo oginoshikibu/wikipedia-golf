b5f3f19e9568736e3c8318f8ad923b2105ed5426
oginoshikibu <oginoshikibu@gmail.com>
2024-01-09 23:51:19 +0900

    refactoring

--- resources/js/Pages/Play.jsx
+++ resources/js/Pages/Play.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Head } from '@inertiajs/react';
 
+
 export default function Play({ const: startPageTitle = "ゲーム", const: goalPageTitle = "ゴルフ" }) {
 
-    // https://www.mediawiki.org/wiki/API:REST_API/Reference/ja#HTMLの取得 よりコピペしたものを改変
     const wikiFetch = async (title) => {
         let url = encodeURI(`https://ja.wikipedia.org/w/rest.php/v1/page/${title}/with_html`);
-        let headers = { 'Api-User-Agent': 'MediaWiki REST API docs examples/0.1 (https://www.mediawiki.org/wiki/API_talk:REST_API)' }
-        const rsp = await fetch(url, headers);
+        const rsp = await fetch(url);
         const data = await rsp.json();
         return data;
     }
@@ -20,47 +19,32 @@ export default function Play({ const: startPageTitle = "ゲーム", const: goalP
             console.error(err.message);
         }
     }
+    
+    
+    const [currentPageHtml, setCurrentPageHtml] = useState(null);
+    const [currentPageTitle, setCurrentPageTitle] = useState(null);
+    const [currentScore, setCurrentScore] = useState(0);
 
-    const modifyHtmlLinks = (html) => {
-        // htmlの中のリンクを変更する
-        // 1. htmlをパースする
-        // 2. リンクのhrefを変更する
-        // 3. 変更したhtmlを返す
-        return html;
+
+    const updateCurrentPage = async (title) => {
+        let result = await wikiFetchAsync(title);
+        setCurrentPageHtml(result.html);
+        setCurrentPageTitle(result.title);
+        setCurrentScore(currentScore + 1);
     }
 
-    const getWikiPage = async (title) => {
-        // media wiki apiを叩いて、titleのページのhtmlを取得する
-        // https://www.mediawiki.org/wiki/API:Main_page
-        const page = new Object();
-        page.title = title;
-        page.html = modifyHtmlLinks((await wikiFetchAsync(title)).html);
-        return page;
-    }
-
-    const [currentPage, setCurrentPage] = useState(null);
-    const [currentStrokes, setCurrentStrokes] = useState(0);
-
+    // init
     useEffect(() => {
-        const fetchData = async () => {
-            const page = await getWikiPage(startPageTitle);
-            setCurrentPage(page);
-        };
-
-        fetchData();
+        updateCurrentPage(startPageTitle);
     }, [startPageTitle]);
 
-    const onLinkClickToHit = async (e) => {
-        setCurrentStrokes(currentStrokes + 1);
-        const page = await getWikiPage(e.target.title);
-        setCurrentPage(page);
-    }
 
+    // judge goal
     useEffect(() => {
-        if (currentPage && currentPage.title === goalPageTitle) {
-            alert(currentStrokes + "打でゴールしました");
+        if (currentPageTitle && currentPageTitle === goalPageTitle) {
+            alert(currentScore + "打でゴールしました");
         }
-    }, [currentPage]);
+    }, [currentPageTitle]);
 
 
     return (
@@ -68,12 +52,12 @@ export default function Play({ const: startPageTitle = "ゲーム", const: goalP
             <Head title="Play" />
             <div className='text-center w-[100%] m-3'>
                 <p>start page: {startPageTitle} {"→"} goal page: {goalPageTitle}</p>
-                <p>現在の打数: {currentStrokes} 打</p>
+                <p>現在の打数: {currentScore} 打</p>
             </div>
-            {currentPage && (
+            {currentPageTitle && (
                 <>
-                    <div>{currentPage.title}</div>
-                    <iframe srcDoc={currentPage.html} className="m-4" width="100%" tabIndex={-1} />
+                    <div>{currentPageTitle}</div>
+                    <iframe srcDoc={currentPageHtml} title={currentPageTitle} className="m-4" width="100%" height={100000} tabIndex={-1} />
                 </>
             )}
         </>
