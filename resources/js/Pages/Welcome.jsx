2cf3053006f87cd390ce6f6b782019f8da3091b0
oginoshikibu <oginoshikibu@gmail.com>
2024-01-21 07:42:34 +0900

    debag用log削除

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,7 +1,7 @@
 import { Link, Head } from '@inertiajs/react';
 
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
-    console.log(auth);
+
     return (
         <>
             <Head title="Welcome" />
