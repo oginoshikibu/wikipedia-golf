936428f786ea121b3bce3c5fab42f35d249c7293
oginoshikibu <oginoshikibu@gmail.com>
2024-01-23 04:42:16 +0900

    確認してtimeout追加

--- app/Http/Controllers/RandomPlayController.php
+++ app/Http/Controllers/RandomPlayController.php
@@ -10,14 +10,12 @@ class RandomPlayController extends Controller
 {
     public function play()
     {
-        // $mediawikiService = new MediawikiService();
-        // $twoRandomPageTitles = $mediawikiService->getRandomJaWikiPagesTitles(2);   
+        $mediawikiService = new MediawikiService();
+        $twoRandomPageTitles = $mediawikiService->getRandomJaWikiPagesTitles(2);   
         return Inertia::render('Play',
             [
-                'startPageTitle' => "こんにちは",
-                'goalPageTitle' => "さようなら",
-                // 'startPageTitle' => $twoRandomPageTitles[0],
-                // 'goalPageTitle' => $twoRandomPageTitles[1],
+                'startPageTitle' => $twoRandomPageTitles[0],
+                'goalPageTitle' => $twoRandomPageTitles[1],
             ]
     );
     }
--- app/Services/MediawikiService.php
+++ app/Services/MediawikiService.php
@@ -24,7 +24,6 @@ class MediawikiService {
 
             MIT License
         */
-
         $endPoint = "https://ja.wikipedia.org/w/api.php";
         $params = [
             "action" => "query",
@@ -38,19 +37,38 @@ class MediawikiService {
 
         $ch = curl_init( $url );
         curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $ch, CURLOPT_TIMEOUT, 10 ); // Set timeout value in seconds
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10); // Set connect timeout value in seconds
         $output = curl_exec( $ch );
+
+        if ($output === false) {
+            throw new \Exception('Failed to fetch data from the API: ' . curl_error($ch));
+        }
+
+        if (curl_getinfo($ch, CURLINFO_HTTP_CODE) === 0) {
+            throw new \Exception('Request to the API timed out.');
+        }
+
         curl_close( $ch );
 
         $result = json_decode( $output, true );
 
-        // Return the response
+        if ($result === null) {
+            throw new \Exception('Failed to decode API response: ' . json_last_error_msg());
+        }
+
         return $result;
     }
 }
 
-// //debug
-// $mediawikiService = new MediawikiService();
-// $titles = $mediawikiService->getRandomJaWikiPagesTitles(5);
-// var_dump($titles);
+if (basename(__FILE__) == basename($_SERVER['PHP_SELF'])) {
+    // test code 
+    $time_start = microtime(true);
+    $mediawikiService = new MediawikiService();
+    $titles = $mediawikiService->getRandomJaWikiPagesTitles(5);
+    $time_end = microtime(true);
+    var_dump($titles);
+    var_dump($time_end - $time_start);
+}
 
 
