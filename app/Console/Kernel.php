cbd2dc75ef709268a0a6978b182c92542ffc00e2
oginoshikibu <oginoshikibu@gmail.com>
2024-01-25 02:05:16 +0900

    公式を参考に仮実装

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Services\MediawikiService;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +15,13 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $mediawikiService = new MediawikiService();
+            $twoRandomPageTitles = $mediawikiService->getRandomJaWikiPagesTitles(2);
+            // log
+            Log::info('twoRandomPageTitles[0] = ' . $twoRandomPageTitles[0]);
+            Log::info('twoRandomPageTitles[1] = ' . $twoRandomPageTitles[1]);
+        })->daily();
     }
 
     /**
@@ -20,7 +29,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
