97ebba43bc7f095480195e56936175e5a291ceec
oginoshikibu <oginoshikibu@gmail.com>
2024-01-30 16:45:02 +0900

    ページ履歴のarr,stack実装

--- resources/js/Pages/Play.jsx
+++ resources/js/Pages/Play.jsx
@@ -2,21 +2,28 @@ import React, { useEffect, useState } from 'react';
 import { Head } from '@inertiajs/react';
 import wikiPageViewer from '@/Components/wikiPageViewer';
 import ErrorBoundary from '@/Components/ErrorBoundary';
+import PrimaryButton from '@/Components/PrimaryButton';
 
 export default function Play({auth, startPageTitle, goalPageTitle, questionId = null}) {
 
     const [currentPageTitle, setCurrentPageTitle] = useState(null);
     const [currentScore, setCurrentScore] = useState(-1);
+    const [playHistory, setPlayHistory] = useState([]);
+    const [playHistoryStack, setPlayHistoryStack] = useState([]);
 
 
     const updateCurrentPage = async (title) => {
         setCurrentPageTitle(title);
         setCurrentScore(currentScore + 1);
+        setPlayHistory([...playHistory, title]);
+        setPlayHistoryStack([...playHistoryStack, title]);
     }
 
     // init
     useEffect(() => {
         updateCurrentPage(startPageTitle);
+        setPlayHistory([startPageTitle]);
+        setPlayHistoryStack([startPageTitle]);
     }, [startPageTitle]);
 
 
@@ -31,6 +38,13 @@ export default function Play({auth, startPageTitle, goalPageTitle, questionId = 
         <ErrorBoundary>
             <>
                 <Head title="Play" />
+                <div>
+                    {playHistory}
+                </div>
+                <div>
+                    {playHistoryStack}
+                </div>
+                
                 <div className='text-center m-3'>
                     <p>start page: {startPageTitle} {"→"} goal page: {goalPageTitle}</p>
                     <p>現在のページ: {currentPageTitle}, 現在の打数: {currentScore} 打</p>
