8e337df9c08f873dccbff28053b65c8d2b3a27f0
oginoshikibu <oginoshikibu@gmail.com>
2024-02-06 05:34:23 +0900

    /に適当にwelcomeと命名

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('Welcome');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
