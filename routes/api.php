77ebd2f0cbc0706db99f739be82be14f5b49e10f
oginoshikibu <oginoshikibu@gmail.com>
2024-02-06 01:50:06 +0900

    踏み台apiのルーティング

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Services\MediawikiService;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/wikiHTML', [MediawikiService::class, 'fetchJaPagesHTMLFromTitle'])->name('wikiHTML');
