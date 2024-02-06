8e82a9d428a0595459ea0890a7ec2bbcc28408ed
oginoshikibu <oginoshikibu@gmail.com>
2024-02-06 17:19:43 +0900

    styleをいろいろと変更

--- resources/js/Pages/Play.jsx
+++ resources/js/Pages/Play.jsx
@@ -63,10 +63,11 @@ export default function Play({ auth, startPageTitle, goalPageTitle, questionId =
         <ErrorBoundary>
             <>
                 <Head title="Play" />
-                <Header auth={auth} />
+                <Header auth={auth}>
+                    <p>現在のページ: {currentPageTitle}, 現在の打数: {currentScore} 打</p>
+                </Header>
                 <div className='text-center m-3'>
                     <p>start page: {startPageTitle} {"→"} goal page: {goalPageTitle}</p>
-                    <p>現在のページ: {currentPageTitle}, 現在の打数: {currentScore} 打</p>
                 </div>
                 <div className='justify-center m-3'>
                     <div className='mb-3'>
