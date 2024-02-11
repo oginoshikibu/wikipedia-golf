a60000179457eb96e581d51437f402a4f69bae6b
oginoshikibu <oginoshikibu@gmail.com>
2024-02-11 16:35:34 +0900

    accountCircleとりあえず作成

---
+++ resources/js/Components/AccountCircle.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "@inertiajs/react";
+
+export default function AccountCircle({ auth }) {
+    return (
+        <img src="img/account_circle_FILL0_wght400_GRAD0_opsz24.svg"/>
+    )
+
+}
