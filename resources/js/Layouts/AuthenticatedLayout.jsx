dfe669be1db7bcd61d168b012def75855aa6a3d8
oginoshikibu <oginoshikibu@gmail.com>
2024-02-12 02:11:27 +0900

    navlinkにprofileの追加

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -24,6 +24,9 @@ export default function Authenticated({ user, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('profile.edit')} active={route().current('profile.edit')}>
+                                    Profile
+                                </NavLink>
                             </div>
                         </div>
 
