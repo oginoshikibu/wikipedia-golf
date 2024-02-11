968ddc79591c6e4783a76657144798bd31f69466
oginoshikibu <oginoshikibu@gmail.com>
2024-02-12 02:50:28 +0900

    headerの仕様変更

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import AccountCircle from './AccountCircle';
 import ApplicationLogo from './ApplicationLogo';
 
-export default function Header({ auth }) {
+export default function Header({ auth, children }) {
     return (
         <>
             <div className="flex w-full h-16 bg-white border-b-2 z-50">
                 <ApplicationLogo className='h-14 m-1 ml-3 flex-none' />
-                <div className="flex-1 flex justify-center items-center text-3xl font-bold">
-                    Wikipedia Golf
-                </div>
-                <div className='flex-2 flex items-center h-16 mr-3'>
+                {children}
+                <div className='flex items-center h-16 mr-3 ml-auto'>
                     <AccountCircle auth={auth} className='h-14' />
                 </div>
             </div>
